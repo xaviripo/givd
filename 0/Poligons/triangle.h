e2fe9908c2aab3189514e9a5f456856625597213
Xavier Ripoll <xaviripo97@gmail.com>
2018-02-20 20:09:33 +0100

    Added missing declarations in Triangle

--- 0/Poligons/triangle.h
+++ 0/Poligons/triangle.h
@@ -9,6 +9,10 @@ public:
     Triangle(glm::vec4, glm::vec4, glm::vec4);
     glm::vec4 obteNormal();
     float obteAngle(int id_vertex);
+    void traslada(glm::vec3 desplazament);
+    void escala(glm::vec3 factor_escalat);
+    void rota(float radians, glm::vec3 punt_rotacio, glm::vec3 eix_rotacio);
+
 };
 
 #endif // TRIANGLE_H
